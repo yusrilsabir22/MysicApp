ad856948539e24f1cd98915a036a3f8c43be064f
yusril <yusril@nubela.co>
2023-06-26 19:31:08 +0700

    Initial Commit

---
+++ MysicApp/MysicAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MysicAppApp.swift
+//  MysicApp
+//
+//  Created by mac on 26/06/23.
+//
+
+import SwiftUI
+
+@main
+struct MysicAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
